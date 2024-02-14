133ff22739831cc30b39f578948517a50b1db0f9
Harsha979 <dharshavardhanr@gmail.com>
2024-02-14 23:35:52 +0530

    hello

---
+++ src/Apple.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Apple(){
+
+    return(
+        <div>
+            helllo
+        </div>
+    )
+}
+
+export default Apple;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Apple from './Apple';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Apple />
   </React.StrictMode>
 );
 
